3baacbdb4e21ba3b3dbf33cfeb31dbde08c8ef70
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-09-07 15:55:36 +0530

    Configured Sponsor page according to new data structure

--- lib/home/sponsor.dart
+++ lib/home/sponsor.dart
@@ -1,27 +1,27 @@
 class Sponsor {
   String name;
-  // String image;
+  String image;
   String desc;
-  String redirectUrl;
+  String url;
   String logo;
 
-  Sponsor({this.name, this.desc, this.redirectUrl, this.logo});
+  Sponsor({this.name, this.desc, this.url, this.logo});
 
   Sponsor.fromJson(Map<String, dynamic> json) {
     name = json['name'];
-    // image = json['image'];
+    image = json['image'];
     desc = json['desc'];
-    redirectUrl = json['redirectUrl'];
-    logo = json['logo'];
+    url = json['url'];
+    // logo = json['logo'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
     data['name'] = this.name;
-    // data['image'] = this.image;
+    data['image'] = this.image;
     data['desc'] = this.desc;
-    data['redirectUrl'] = this.redirectUrl;
-    data['logo'] = this.logo;
+    data['url'] = this.url;
+    // data['logo'] = this.logo;
     return data;
   }
 }
