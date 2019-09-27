d3bd51ca9eaabaed22369c3a5a0a9b3af7c6efec
ashutoshsingh05 <201851029@iiitvadodara.ac.in>
2019-09-28 03:33:21 +0530

    Fix Sponsor page theme to dark

--- lib/sponsors/sponsor_page.dart
+++ lib/sponsors/sponsor_page.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:devfest_gandhinagar/dialogs/error_dialog.dart';
 import 'package:devfest_gandhinagar/home/sponsor.dart';
@@ -17,61 +16,64 @@ class SponsorPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // var _homeBloc = HomeBloc();
     return DevScaffold(
       title: "Sponsors",
-      body: FutureBuilder<DocumentSnapshot>(
-        future:
-            Firestore.instance.collection("homepage").document("data").get(),
-        builder:
-            (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
-          if (snapshot.connectionState != ConnectionState.done) {
-            return Container(
-              color: Theme.of(context).scaffoldBackgroundColor,
-              child: Center(
-                child: SpinKitChasingDots(
-                  color: Tools.multiColors[Random().nextInt(3)],
-                ),
-              ),
-            );
-          } else {
-            if (snapshot.hasError) {
-              return Center(
-                child: ErrorDialog(
-                  error: snapshot.error,
-                  child: Text(Devfest.some_error_text),
+      body: Container(
+        color: Colors.black,
+        child: FutureBuilder<DocumentSnapshot>(
+          future:
+              Firestore.instance.collection("homepage").document("data").get(),
+          builder:
+              (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+            if (snapshot.connectionState != ConnectionState.done) {
+              return Container(
+                color: Theme.of(context).scaffoldBackgroundColor,
+                child: Center(
+                  child: SpinKitChasingDots(
+                    color: Tools.multiColors[Random().nextInt(3)],
+                  ),
                 ),
               );
             } else {
-              Map<String, List<dynamic>> map = Map<String, List<dynamic>>.from(
-                  snapshot.data.data["sponsors"]);
-
-              int count = 0;
-              for (int i = 0; i < map.length; i++) {
-                String sponsorCategory = map.keys.toList()[i];
-                for (int j = 0; j < map[sponsorCategory].length; j++) {
-                  sponsorsList
-                      .add(Sponsor.fromJson(Map.from(map[sponsorCategory][j])));
-                  count++;
-                }
-              }
-              if (sponsorsList.length < 1) {
+              if (snapshot.hasError) {
                 return Center(
-                  child: Text(Devfest.comingSoonText),
+                  child: ErrorDialog(
+                    error: snapshot.error,
+                    child: Text(Devfest.some_error_text),
+                  ),
+                );
+              } else {
+                Map<String, List<dynamic>> map =
+                    Map<String, List<dynamic>>.from(
+                        snapshot.data.data["sponsors"]);
+
+                int count = 0;
+                for (int i = 0; i < map.length; i++) {
+                  String sponsorCategory = map.keys.toList()[i];
+                  for (int j = 0; j < map[sponsorCategory].length; j++) {
+                    sponsorsList.add(
+                        Sponsor.fromJson(Map.from(map[sponsorCategory][j])));
+                    count++;
+                  }
+                }
+                if (sponsorsList.length < 1) {
+                  return Center(
+                    child: Text(Devfest.comingSoonText),
+                  );
+                }
+                return ListView.builder(
+                  itemCount: count,
+                  itemBuilder: (BuildContext context, int i) {
+                    return SponsorImage(
+                      imgUrl: sponsorsList[i].image,
+                      url: sponsorsList[i].url,
+                    );
+                  },
                 );
               }
-              return ListView.builder(
-                itemCount: count,
-                itemBuilder: (BuildContext context, int i) {
-                  return SponsorImage(
-                    imgUrl: sponsorsList[i].image,
-                    url: sponsorsList[i].url,
-                  );
-                },
-              );
             }
-          }
-        },
+          },
+        ),
       ),
     );
   }
@@ -89,6 +91,7 @@ class SponsorImage extends StatelessWidget {
         launch(url);
       },
       child: Card(
+        color: Colors.transparent,
         elevation: 0.0,
         child: Padding(
           padding: const EdgeInsets.all(12.0),
